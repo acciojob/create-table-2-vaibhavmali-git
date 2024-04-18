08689b8a6505105d9110e08fe944d0b8671960f4
Vaibhav Rameshwar Mali <malivaibhav.7620@gmail.com>
2024-04-18 09:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
-    //Write your code here
-  
+    let rn = prompt('Input number of rows');
+    let cn = prompt('Input number of columns');
+
+    let table = document.getElementById('myTable');
+
+    // Clear out the existing table data
+    table.innerHTML = "";
+
+    for(let i=0; i<parseInt(rn, 10); i++) {
+        let row = document.createElement('tr');
+
+        for(let j=0; j<parseInt(cn, 10); j++) {
+            let cell = document.createElement('td');
+            cell.textContent = `Row-${i} Column-${j}`;
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
 }
